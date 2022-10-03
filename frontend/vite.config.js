66e82f589d309a736e89ff97d804d25528572699
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-10-03 23:02:51 +0800

    alias src to @ config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,27 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: { //https://vitejs.dev/guide/build.html
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        staff: resolve(__dirname, 'staff/index.html'),
-        setLearningJourney: resolve(__dirname, 'setLearningJourney.html'),
-        staff_create: resolve(__dirname, 'staff/create_lj.html'),
-        staff_create_2: resolve(__dirname, 'staff/create_lj_2.html'),
-        managerJobRole: resolve(__dirname, 'managerJobRole.html'),
-        jobRoleDetails: resolve(__dirname, 'jobRoleDetails.html'),
-      }
-    }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
+  // build: { //https://vitejs.dev/guide/build.html
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       staff: resolve(__dirname, 'staff/index.html'),
+  //       setLearningJourney: resolve(__dirname, 'setLearningJourney.html'),
+  //       staff_create: resolve(__dirname, 'staff/create_lj.html'),
+  //       staff_create_2: resolve(__dirname, 'staff/create_lj_2.html'),
+  //       managerJobRole: resolve(__dirname, 'managerJobRole.html'),
+  //       jobRoleDetails: resolve(__dirname, 'jobRoleDetails.html'),
+  //     }
+  //   }
+  // }
 })
