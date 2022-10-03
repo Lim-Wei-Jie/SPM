6e1da16519e5d291c1ac11f1990c811fe9bc3beb
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-10-03 23:21:56 +0800

    create homepage and login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 // import components from the views folder to be used here
+import HomePage from "../views/HomePage.vue"
+import LogIn from "../views/LogIn.vue"
 
 const routes = [
-    /* 
-    Each { } is for each page, e.g.,
     {
-        path: "/", (URL name behind localhost:3000)
-        name: "homepage", (give any name)
-        component: HomePage (name of component in the views folder)
-    }
-    */
-    {
-
+        path: '/',
+        component: HomePage
     },
 
     {
-
+        path: '/login',
+        component: LogIn
     }
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHistory(),
     routes,
 });
 
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- eslint-disable -->
+
+    <!-- Hero Container -->
+    <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Hello there</h1>
+                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <label for="my-modal-4" class="btn modal-button">LOG IN</label>
+            </div>
+        </div>
+    </div>
+
+    <!-- Log In Modal -->
+    <!-- <input type="checkbox" id="my-modal-4" class="modal-toggle" />
+    <label for="my-modal-4" class="modal cursor-pointer">
+        <label class="modal-box relative" for="">
+            <h3 class="text-lg font-bold">Log In</h3>
+        </label>
+    </label> -->
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
