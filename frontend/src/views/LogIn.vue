5059bfca6f23953d101dba0b5c2c0ae1180c0810
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-10-04 21:56:11 +0800

    add log in function
    log in page, card component for each role

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Login</h1>
+<!-- eslint-disable -->
+    <div class="container mx-auto flex items-center h-screen">
+        <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 grow">
+            <LoginCard :role="staff" />
+            <LoginCard :role="manager" />
+            <LoginCard :role="hr" />
+        </div>
+    </div>
 </template>
+
+<script setup>
+import LoginCard from '@/components/LoginCard.vue'
+
+const staff = 'Staff'
+const manager = 'Manager'
+const hr = 'Human Resource'
+
+</script>
+
+<style scoped>
+
+</style>
