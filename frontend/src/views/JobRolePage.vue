074620db3675aba33ba244a2d029abd37ebe6890
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-10-05 18:12:59 +0800

    add job role page

---
+++ frontend/src/views/JobRolePage.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- eslint-disable -->
+    <NavBar/>
+    {{jobRole}}
+</template>
+
+<script setup>
+import NavBar from '@/components/Navbar.vue'
+
+const props = defineProps({
+    jobRole: {
+        type: String,
+        default: ''
+    }
+});
+</script>
+
+<style scoped>
+</style>
