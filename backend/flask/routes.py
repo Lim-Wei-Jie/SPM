9beb5e364988c879a42c29b65abbc8f40d5b178a
fierybean <norman.ng.2020@smu.edu.sg>
2022-10-10 01:26:14 +0800

    Attempt at skill assignment
    Flask Circular error present for skill_assignment
    
    Seperated db,routing and app, but now has sqlalchemy.exc.InterfaceError

--- backend/flask/routes.py
+++ backend/flask/routes.py
@@ -1,53 +1,17 @@
-from tkinter import CASCADE
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from datetime import datetime
-from sqlalchemy.orm import relationship,backref
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/lms'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-CORS(app)
+from app import app
+from db import *
+from flask import request, jsonify
 
 
-#association table + basic models
-
-
-class Course(db.Model):
-    __tablename__ = 'course'
-
-    Course_ID = db.Column(db.String(20), primary_key=True)
-    Course_Name = db.Column(db.String(50), nullable=False)
-    Course_Desc = db.Column(db.String(255) , nullable=False)
-    Course_Status = db.Column(db.String(15), nullable=False)
-    Course_Type = db.Column(db.String(10), nullable=False)
-    Course_Category = db.Column(db.String(50), nullable=False) 
-  
-
-    def json(self):
-        return {"course_id": self.Course_ID, "Course_Name": self.Course_Name, "Course_Desc": self.Course_Desc, "Course_Status": self.Course_Status,"Course_Type": self.Course_Type, "Course_Category":self.Course_Category}
-
-
-
-class Skill(db.Model):
-    __tablename__ = 'Skill'
-
-    
-    Skill_ID = db.Column(db.Integer, primary_key=True)
-    Skill_Name = db.Column(db.String(64), nullable=False)
-    Skill_Desc = db.Column(db.String(255), nullable=False)
-    Date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-
-
-    def json(self):
-        return {"Skill_id": self.Skill_ID , "Skill_name": self.Skill_Name, "Skill_desc": self.Skill_Desc , "Skill_status": self.Date_created}
-
-
-
+""" @app.route('/index/')
+def hello():
+    return jsonify(
+    {
+        "code": 690,
+        "message": [skill_assignment.c.keys()]
+    }
+    ), 404
+ """
 
 #https://teamtreehouse.com/community/what-is-the-difference-between-json-and-jsonparse#:~:text=The%20difference%20is%3A,)%20JavaScript%20object(s).
 @app.route("/Skill")
