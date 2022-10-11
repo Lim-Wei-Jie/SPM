83a2ac37a0ffc6ca2309925d8a158435589159aa
fierybean <norman.ng.2020@smu.edu.sg>
2022-10-11 22:16:05 +0800

    Skill_Assignment Creation
    Mapped skill to course using existing routes as template

--- backend/manager.py
+++ backend/manager.py
@@ -7,6 +7,9 @@ from flask_cors import CORS
 import json
 from os import environ
 
+#new
+from datetime import datetime
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/lms'
@@ -131,6 +134,32 @@ class Skill_Assign(db.Model):
  
     def json(self):
         return {"skill_assignment_id": self.skill_assignment_id, "Course_ID": self.Course_ID, "Skill_ID": self.Skill_ID} 
+
+
+
+class Skill(db.Model):
+
+    __tablename__ = 'Skill'
+    
+    Skill_ID = db.Column(db.Integer, primary_key=True)
+    Skill_Name = db.Column(db.String(64), nullable=False)
+    Skill_Desc = db.Column(db.String(255), nullable=False)
+    Date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+   #Courses = relationship("Course", secondary=skill_assignment)
+    def json(self):
+
+        return {"Skill_id": self.Skill_ID , "Skill_name": self.Skill_Name, "Skill_desc": self.Skill_Desc , "Date_created": self.Date_created}
+    
+    
+    def __init__(self, Skill_ID, Skill_Name, Skill_Desc, Date_created=None):
+        self.Skill_ID = Skill_ID 
+        self.Skill_Name = Skill_Name
+        self.Skill_Desc = Skill_Desc
+        if Date_created is None:
+            self.created_time = datetime.utcnow()
+        else:
+            self.created_time = Date_created
+    
 
 
 # show all inventory
@@ -350,8 +379,160 @@ def get_course_id_by_skill(Skill_ID):
         }
     ), 404
 
+
+#for skill-course assignment
+
+
+@app.route("/Skill")
+def get_all_skill():
+    #list
+    Skilllist = Skill.query.all()
+    if len(Skilllist):
+        
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    
+                    #turn python data into javascript object
+                    "books": [Skill.json() for Skill in Skilllist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no Skills."
+        }
+    ), 404
+    
+
+
+
+@app.route("/skill/<string:Skill_ID>/<string:Skill_Name>/<string:Skill_Desc>/<string:Date_created>", methods=['GET','POST'])
+def create_skill(Skill_ID,Skill_Name,Skill_Desc,Date_created):
+
+    if (Skill.query.filter_by(Skill_ID = Skill_ID ).first()):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "Skill_ID": Skill_ID,
+                    "Skill_Name": Skill_Name,
+                    "Skill_Name": Skill_Desc,
+                    "Date_created":Date_created
+                },
+                "message": "Skill already exists."
+            }
+        ), 400
+ 
+    #data = request.get_json()
+    #print("poopo" + data)
+    new_skill = Skill(Skill_ID,Skill_Name,Skill_Desc,Date_created)
+ 
+    try:
+        db.session.add(new_skill)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "Skill_ID": Skill_ID,
+                    "Skill_Name": Skill_Name,
+                    "Skill_Name": Skill_Desc,
+                    "Date_created":Date_created
+                },
+                "message": "An error occurred creating the Skill."
+            }
+        ), 500
+ 
+    return jsonify(
+        {
+            "code": 201,
+            "data": new_skill.json()
+        }
+    ), 201 
+    
+
+#mapping skills to courses
+
+@app.route("/skill/<string:skill_assignment_id>/<string:Course_ID>/<string:Skill_ID>/", methods=['GET','POST'])
+def skill_to_course_assignment(skill_assignment_id,Skill_ID,Course_ID):
+
+    if (Skill_Assign.query.filter_by(skill_assignment_id = skill_assignment_id ).first()):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "Skill_Assignment_ID": skill_assignment_id,
+                    "Skill_ID": Skill_ID,
+                    "Course_ID": Course_ID
+                    
+                },
+                "message": "Skill already exists."
+            }
+        ), 400
+ 
+    #data = request.get_json()
+    #print("poopo" + data)
+    new_skill_assignment = Skill_Assign(skill_assignment_id,Course_ID,Skill_ID)
+ 
+    try:
+        db.session.add(new_skill_assignment)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "Skill_Assignment_ID": skill_assignment_id,
+                    "Skill_ID": Skill_ID,
+                    "Course_ID": Course_ID
+                },
+                "message": "An error occurred when assigning the skill to course."
+            }
+        ), 500
+ 
+    return jsonify(
+        {
+            "code": 201,
+            "data": new_skill_assignment.json()
+        }
+    ), 201 
+    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004, debug=True)
- 
+
+
+
+
+#viewing the skill_assignment table
+
+"""
+@app.route("/Skill_Course_Assignment")
+def get_mapped_skill_to_course():
+    #list
+    Skill_Course_Map = Skill_Assign.query.all()
+    if len(Skill_Course_Map):
+        
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    
+                    #turn python data into javascript object
+                    "maps": [Skill_Assign.json() for Skill_Assign in Skill_Course_Map]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no Skills assigned to Courses."
+        }
+    ), 404
+ """
 
     
