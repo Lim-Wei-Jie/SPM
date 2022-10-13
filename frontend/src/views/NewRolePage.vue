c48c9455a8ab0cadf70c79500601585e75be1db4
celestelxt <xtlee.2020@smu.edu.sg>
2022-10-13 15:26:31 +0800

    update

--- frontend/src/views/NewRolePage.vue
+++ frontend/src/views/NewRolePage.vue
@@ -5,7 +5,7 @@
         <NavBar/>
 
         <!-- Hero Container -->
-        <form action="">
+        <!-- <form action=""> -->
             <div class="container mx-auto my-8">
                 <div class="auto-row-max space-y-2">
                     <div id="j-info" class="space-y-2">
@@ -18,7 +18,7 @@
                         <div id="j-des" class="space-y-2 w-4/6">
                             
                             <h3 class="font-bold text-xl">Job Description</h3>
-                                <input type="text" id="job-des" name="job-des" class="text-xl bg-gray-100 py-2" placeholder="Enter Job Description here..." style="width:700px"  v-model="new_job_des">
+                                <input type="text" id="job-des" name="job-des" class="text-xl  bg-gray-100 py-2" placeholder="Enter Job Description here..." style="width:700px; height:200px;" v-model="new_job_des" >
                            
                         </div>  
                     </div>
@@ -40,7 +40,7 @@
                             <div class="card w-80 bg-base-100 shadow-xl">
                                 <fieldset>      
                                     <!-- <legend>What is Your Favorite Pet?</legend>       -->
-                                    <input type="checkbox" v-bind:name="skill.Skill_name" value="Cats" class="m-3">{{skill.Skill_name}} <br>    
+                                    <input type="checkbox" v-bind:value="skill.Skill_name" v-bind:id="skill.Skill_id" v-model="selectedSkill" class="m-3">{{skill.Skill_name}} <br>    
                                     <!-- <input type="submit" value="Submit now" />       -->
                                 </fieldset> 
                             </div>           
@@ -58,34 +58,35 @@
                                    
                            
                             <!-- courses will be displayed when a skill is selected-->
-                            <div class="w-32" v-if="selectedSkill">
-                            <div class="grid grid-cols-3 gap-4 ml-3 mb-5">
+                            <!-- <div class="w-32" v-if="selectedSkill">
+                            <div class="grid grid-cols-3 gap-4 ml-3 mb-5"> -->
                                 <!--Card #1 -->
-                                <div class="card w-80 bg-base-100 shadow-xl" v-for="course in skill.Skill_courses">
+                                <!-- <div class="card w-80 bg-base-100 shadow-xl" v-for="course in skill.Skill_courses">
                                     <div class="card-body">
                                         <div id="top-card" class="flex-row">
                                             <h2 class="card-title mb-2">{{course.course_id}}: {{course.Course_Name}}</h2>
                                             <h4>{{course.Course_Desc}}</h4>
                                             
-                                        </div>
+                                        </div> -->
                                         <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
                                         <!-- <div class="card-actions justify-end">
                                             <button class="btn btn-primary">Buy Now</button>
                                         </div> -->
-                                    </div>
-                                </div>   
-                        </div>
-                        </div>
+                                    <!-- </div>
+                                </div>    -->
+                        <!-- </div>
+                        </div> -->
                     </div>
                 </div>
                 <div class="w-32 place-self-center">
                     <!--need to have v-on to delete job role from database-->
-                    <button class="btn" v-on:click="createRole(newJobRoleData)">Create Job Role</button>
+                    <!-- v-on:click="createRole(newJobRoleData)" -->
+                    <button class="btn"  v-on:click="display()">Create Job Role</button>
                 </div>
             </div>
             </div>
             </div>
-        </form>
+        <!-- </form> -->
     </div>
 </template>
 
@@ -93,8 +94,11 @@
 import NavBar from '@/components/Navbar.vue'
 import { ref, toRefs, onBeforeMount } from 'vue'
 import { createRole } from '@/endpoint/endpoint.js'
-import { getCourses } from '@/endpoint/endpoint.js'
 
+const new_job_title = ref();
+const new_job_des = ref();
+const newJobRoleData = ref([]);
+const selectedSkill = ref([]);
 
 const skills = ref([
       {
@@ -110,8 +114,12 @@ const skills = ref([
         "Skill_status": null
       }
     ]);
-const checkboxes = ref([]);
 
+function display(){
+   
+    this.newJobRoleData.push(this.new_job_title, this.new_job_des, this.selectedSkill);
+    console.log(this.newJobRoleData);
+}
 
 
 
