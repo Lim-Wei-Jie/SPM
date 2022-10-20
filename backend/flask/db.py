9be509db6b76178e457ae2847235fa6fd6f86d45
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2022-10-20 11:02:25 +0800

    registration by staff id
    trying to get registration to include role name

--- backend/flask/db.py
+++ backend/flask/db.py
@@ -41,6 +41,8 @@ class Skill(db.Model):
     Skill_Desc = db.Column(db.String(255), nullable=False)
     Date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
    #Courses = relationship("Course", secondary=skill_assignment)
+   
+   
     def json(self):
 
         return {"Skill_id": self.Skill_ID , "Skill_name": self.Skill_Name, "Skill_desc": self.Skill_Desc , "Date_created": self.Date_created}
@@ -64,18 +66,54 @@ class Registration(db.Model):
     Staff_ID = db.Column(db.String(255), nullable=False)
     Reg_Status = db.Column(db.String(64), nullable=False)
     Completion_Status = db.Column(db.String(64), nullable=False)
+    Role_ID = db.Column(db.Integer, nullable=False)
   
 
     def json(self):
-        return {"reg_id": self.Reg_ID, "course_id": self.Course_ID , "staff_id": self.Staff_ID, "reg_status": self.Reg_Status, "completion_status":self.Completion_Status}
+        return {"reg_id": self.Reg_ID, "course_id": self.Course_ID , "staff_id": self.Staff_ID, "reg_status": self.Reg_Status, "completion_status":self.Completion_Status,"Role_ID":self.Role_ID}
     
     
-    def __init__(self, Reg_ID, Course_ID, Staff_ID, Reg_Status,Completion_Status):
+    def __init__(self, Reg_ID, Course_ID, Staff_ID, Reg_Status,Completion_Status,Role_ID):
         self.Reg_ID = Reg_ID 
         self.Course_ID = Course_ID
         self.Staff_ID = Staff_ID
         self.Reg_Status = Reg_Status
         self.Completion_Status = Completion_Status
+        self.Role_ID = Role_ID
+        
+        
+class Role(db.Model):
+    __tablename__ = 'role'
+    # inventoryId = db.Column(db.Integer, primary_key=True)
+    # storeName = db.Column(db.String(64), nullable=False)
+    Role_ID = db.Column(db.Integer, primary_key=True)
+    Role_Name = db.Column(db.String(64), nullable=False, unique=True)
+    Role_Desc = db.Column(db.String(512), nullable=False, unique=True)
+    Date_Created = db.Column(db.String(64), nullable=False, unique=True, default=datetime.utcnow)
+
+
+    # staff_id = db.Column(db.String(64), nullable=False, primary_key=True)
+    #price = db.Column(db.Float(precision=2), nullable=False)
+    #stockCount = db.Column(db.Integer)
+    # imageLink= db.Column(db.String(100), nullable=False)
+    #userId = db.Column(db.Integer, primary_key=True)
+    #username = db.Column(db.String(64), nullable=False, unique=True)
+    
+
+    def __init__(self, Role_ID, Role_Name,Role_Desc,Date_Created):
+        # self.inventoryId = inventoryId
+        # self.storeName = storeName
+        self.Role_ID = Role_ID
+        self.Role_Name = Role_Name
+        self.Role_Desc = Role_Desc
+        self.Date_Created = Date_Created
+
+        # self.imageLink = imageLink
+ 
+    def json(self):
+        # "inventoryId": self.inventoryId,
+        return {"Role_ID": self.Role_ID, "Role_Name": self.Role_Name, "Role_Desc": self.Role_Desc, "Date_Created": self.Date_Created} 
+
     
     
     
