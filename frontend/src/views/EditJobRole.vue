08f185116f9526073a67964dd62e14472357fef4
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-11-06 15:48:33 +0800

    UI fixes

--- frontend/src/views/EditJobRole.vue
+++ frontend/src/views/EditJobRole.vue
@@ -16,15 +16,7 @@
                 </RouterLink>
             </div>
 
-            <div class="grid grid-cols-2 my-8">
-                <p class="text-3xl"> Edit Job Role </p>
-                <!-- Delete button -->
-                <button @click="handleDeleteRole(roleStore.role.roleID)" class="btn btn-circle place-self-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-                    </svg>
-                </button>
-            </div>
+            <p class="text-3xl my-8"> Edit Job Role </p>
 
             <!-- Form component -->
             <form class="form-control space-y-6" @submit.prevent="handleEditRole">
@@ -290,10 +282,6 @@ async function handleEditRole() {
         // error.value = err
         console.log(err);
     }
-}
-
-function handleDeleteRole(roleID) {
-    console.log(roleID);
 }
 
 </script>
--- frontend/src/views/JobRolePage.vue
+++ frontend/src/views/JobRolePage.vue
@@ -20,7 +20,7 @@
             </div>
 
             <!-- Back button -->
-            <div class="mt-4">
+            <div class="my-8">
                 <RouterLink to="/manager">
                     <button class="btn btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -30,16 +30,25 @@
                 </RouterLink>
             </div>
 
-            <div class="grid grid-cols-2 my-4">
+            <div class="grid grid-cols-2">
                 <!-- Role Name -->
                 <p class="text-3xl font-bold underline underline-offset-8">
                     {{roleStore.role.roleName}}
                 </p>
-                <!-- Edit button -->
-                <button @click="handleEditClick(roleStore.role.roleName)" class="btn btn-circle place-self-end">
-                    <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
-                </button>
+                <div class="flex justify-end space-x-2">
+                    <!-- Edit button -->
+                    <button @click="handleEditClick(roleStore.role.roleName)" class="btn btn-circle">
+                        <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
+                    </button>
+                    <!-- Delete button -->
+                    <button @click="handleDeleteRole(roleStore.role.roleID)" class="btn btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
+            
             <!-- Role Desc -->
             <div class="space-y-2 my-4 w-4/6">
                 <p class="font-medium text-lg">
@@ -210,6 +219,10 @@ function handleEditClick(roleName) {
 function handleCourseClick(skillName, courseName) {
     courseModal.courseName = courseName
     courseModal.courseDesc = roleStore.role.coursesBySkillName[skillName].courses[courseName].Course_Desc
+}
+
+function handleDeleteRole(roleID) {
+    console.log(roleID);
 }
 
 </script>
