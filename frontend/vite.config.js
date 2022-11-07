38438685a5de0cb2f0e66ccdfb09998dd6a8588e
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2022-11-07 21:03:19 +0800

    remove code and files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,17 +11,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   }
-  // build: { //https://vitejs.dev/guide/build.html
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html'),
-  //       staff: resolve(__dirname, 'staff/index.html'),
-  //       setLearningJourney: resolve(__dirname, 'setLearningJourney.html'),
-  //       staff_create: resolve(__dirname, 'staff/create_lj.html'),
-  //       staff_create_2: resolve(__dirname, 'staff/create_lj_2.html'),
-  //       managerJobRole: resolve(__dirname, 'managerJobRole.html'),
-  //       jobRoleDetails: resolve(__dirname, 'jobRoleDetails.html'),
-  //     }
-  //   }
-  // }
 })
